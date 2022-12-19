8edb5e0bbc797e4b1039cac0fed20f9628560ea8
Mitch Hollander <mitchell.hollander@bjss.com>
2022-12-18 23:09:31 -0600

    COMPLETE: days 1-14.

--- golang/day13/part2.go
+++ golang/day13/part2.go
@@ -13,35 +13,48 @@ import (
 var EntryType = reflect.TypeOf(0.0)
 var ListType = reflect.TypeOf([]any{})
 
-type Packet []any
-
-func Quicksort(data []Packet, lo int, hi int) {
-
+type Packet struct {
+	val []any
 }
 
-func Partition(data []Packet, lo int, hi int) int {
-	// Default pivot: highest point
-	pivotValue := data[hi]
+func Quicksort(data []Packet, left, right int) {
+	if left < right {
+		p := Partition(data, left, right)
+		Quicksort(data, left, p-1)
+		Quicksort(data, p+1, right)
+	}
+}
 
-	// Swap partitioned elements about the pivot
-	i := lo - 1
-	for j := lo; j < hi; j++ {
-		if !data[j].GreaterThan(pivotValue) {
+func Partition(data []Packet, left, right int) int {
+	// choose the most right element as our pivot
+	pivot := data[right]
+
+	// Swap elements that should go before pivot
+	i := left
+	for j := left; j < right; j++ {
+		if data[j].Before(pivot) {
+			data[i], data[j] = data[j], data[i]
 			i++
-			tmp := &data[i]
-			// TODO
 		}
 	}
+
+	/*
+	 * After swapping every i & j:
+	 *  - everything after i is larger than pivot
+	 *  - everything before i is smaller than pivot
+	 * so, put the pivot right in between.
+	 */
+	data[right], data[i] = data[i], data[right]
 
 	return i
 }
 
-func (left Packet) GreaterThan(right Packet) bool {
+func (left Packet) Before(right Packet) bool {
 	// Set a default value to true, break on contradiction
 	direction := 0
-	PacketCompare(left, right, &direction)
+	PacketCompare(left.val, right.val, &direction)
 
-	if direction == 1 {
+	if direction < 1 {
 		return true
 	} else {
 		return false
@@ -50,41 +63,34 @@ func (left Packet) GreaterThan(right Packet) bool {
 
 func PacketCompare(left any, right any, carry *int) {
 	// Do not iterate deeper if contradiction found
-	if *carry == 1 {
+	if *carry != 0 {
 		return
 	}
 
 	// Get types
 	leftType := reflect.TypeOf(left)
 	rightType := reflect.TypeOf(right)
-	fmt.Printf("%15T %v\n", left, left)
-	fmt.Printf("%15T %v\n\n", right, right)
 
 	if leftType == EntryType && rightType == EntryType {
 		*carry = intcompare(left.(float64), right.(float64))
 	} else if leftType == ListType && rightType == ListType {
 		for i := range left.([]any) {
 			if i >= len(right.([]any)) {
-				// Ran out of entries on right, BAD
+				// Ran out of entries on right,  RIGHT goes first
 				*carry = 1
-				break
+				return
 			}
 			PacketCompare(left.([]any)[i], right.([]any)[i], carry)
-			if *carry == -1 {
-				// Found the left side is smaller, CORRECT (can leave)
-				break
-			} else if *carry == 0 {
-				// Tie, need to continue search
-				continue
-			} else {
-				// Found the left side is larger, BAD (can leave)
-				break
+			if *carry != 0 {
+				// found result, done.
+				return
 			}
 		}
 		// If left side ran out before right side
 		if len(left.([]any)) < len(right.([]any)) {
-			// left side ran out, GOOD
+			// left side ran out, LEFT goes first
 			*carry = -1
+			return
 		}
 	} else if leftType == EntryType && rightType == ListType {
 		PacketCompare([]any{left}, right, carry)
@@ -99,10 +105,13 @@ func PacketCompare(left any, right any, carry *int) {
 
 func intcompare(left float64, right float64) int {
 	if left < right {
+		// LEFT FIRST
 		return -1
 	} else if left == right {
+		// continue
 		return 0
 	} else {
+		// RIGHT FIRST
 		return 1
 	}
 }
@@ -120,25 +129,37 @@ func main() {
 	// process
 	//
 
-	// Inject
-	score := 0
-	for i, packet := range data[0:] {
-		if packet.Compare() {
-			score += i + 1
-			fmt.Printf("++++++ Packet %d is CORRECT\n", i+1)
-		} else {
-			fmt.Printf("------ Packet %d is WRONG\n", i+1)
+	// Inject start and stop signal
+	start := Packet{
+		val: []any{[]any{2.0}},
+	}
+	stop := Packet{
+		val: []any{[]any{6.0}},
+	}
+	data = append(data, start, stop)
+	// Sort
+	Quicksort(data, 0, len(data)-1)
+	// Score
+	score := 1
+	for i, entry := range data {
+		indicator := 0
+		fmt.Printf("%4d %v\n", i, entry)
+		if PacketCompare(entry.val, start.val, &indicator); indicator == 0 {
+			score *= i + 1
+		}
+		indicator = 0
+		if PacketCompare(entry.val, stop.val, &indicator); indicator == 0 {
+			score *= i + 1
 		}
 	}
 
 	//
 	// report
 	//
-
 	fmt.Println(score)
 }
 
-func ParseInput(fileName string) (data []PacketSorter) {
+func ParseInput(fileName string) (data []Packet) {
 	// Open File
 	fin, err := os.Open(fileName)
 	if err != nil {
@@ -149,22 +170,17 @@ func ParseInput(fileName string) (data []PacketSorter) {
 	// Scan to read line by line
 	scanner := bufio.NewScanner(fin)
 
-	data = make([]PacketSorter, 0)
-	for i := 0; scanner.Scan(); i++ {
+	data = make([]Packet, 0)
+	for scanner.Scan() {
 		// Extract
 		line := strings.TrimSpace(scanner.Text())
 		if len(line) == 0 {
-			// Decrement counter and skip this line
-			i--
 			continue
 		}
 		// Transform
-		if i%2 == 0 {
-			data = append(data, PacketSorter{})
-			json.Unmarshal([]byte(line), &(data[i/2].left))
-		} else {
-			json.Unmarshal([]byte(line), &(data[i/2].right))
-		}
+		packet := Packet{}
+		json.Unmarshal([]byte(line), &packet.val)
+		data = append(data, packet)
 	}
 	return
 }
